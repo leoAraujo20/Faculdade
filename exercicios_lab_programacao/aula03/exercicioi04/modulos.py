10021782988dac91a1ebf5eeec369f964d8d8a17
leoAraujo20 <leonidasaraujo19@gmail.com>
2024-08-25 14:27:23 -0300

    start exercise 4

---
+++ exercicios_lab_programacao/aula03/exercicioi04/modulos.py
@@ -0,0 +1,5 @@
+def can_vote(age):
+    if age >= 16:
+        return 'Can vote!'
+    return "Can't vote"
+
---
+++ exercicios_lab_programacao/aula03/exercicioi04/principal.py
@@ -0,0 +1,3 @@
+while True:
+    user_input = int(input('Chose one action:\n[1]Pode votar? \n[2]Ver Categoria: \n[3]Pode alistar? \n[4]Encerrar programa: '))
+    
