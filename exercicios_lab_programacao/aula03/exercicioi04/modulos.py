ea9313de3b7c36f78e3e715670d892eb0ee8523a
leoAraujo20 <leonidasaraujo19@gmail.com>
2024-08-25 20:53:26 -0300

    exercise 4 solution

--- exercicios_lab_programacao/aula03/exercicioi04/modulos.py
+++ exercicios_lab_programacao/aula03/exercicioi04/modulos.py
@@ -3,3 +3,18 @@ def can_vote(age):
         return 'Can vote!'
     return "Can't vote"
 
+def age_category(age):
+    if age <= 11:
+        return 'Child'
+    elif age <= 20:
+        return 'Teenager'
+    elif age <= 30:
+        return 'Young'
+    elif age <= 59:
+        return 'Adult'
+    return 'Elderly'
+
+def enlistment_age(age, gender):
+    if age >= 18 and gender =='M':
+        return 'Can enlist!'
+    return "Can't enlist!"
